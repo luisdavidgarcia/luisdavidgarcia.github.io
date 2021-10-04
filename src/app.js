674535f95f83b1c3bcd1274e5477fdb7d2465196
PowerCodingG <boogus@wmail.com>
2021-10-04 16:58:22 -0700

    Style Changes

--- src/app.js
+++ src/app.js
@@ -15,14 +15,24 @@ const navSlide = () => {
 				link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 4}s';
 			}	
 		});
-	  
 		//Burger Animation
 		burger.classList.toggle('toggle');
-		
 	});
 
 
 }
-
-
 navSlide();
+
+/*
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click',() => {
+	if(!menuOpen){
+		menuBtn.classList.add('open');
+		menuOpen = true;
+	}else{
+		menuBtn.classList.remove('open');
+		menuOpen = false;
+	}	
+});
+*/

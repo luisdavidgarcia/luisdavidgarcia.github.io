5d6bf41f5dadc93fb611a9f540c954ced1ace305
PowerCodingG <boogus@wmail.com>
2021-10-04 17:56:54 -0700

    Style Changes

--- src/app.js
+++ src/app.js
@@ -2,27 +2,36 @@ const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
+	
+	let menuOpen = false;
 
 	//Toggle Nav
 	burger.addEventListener('click',() => {
-		nav.classList.toggle('nav-active');
-		
-		//Animate Links
-		navLinks.forEach((link, index) => {
-			if(link.style.animation){
-				link.style.animation = '';
-			} else {
-				link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 4}s';
-			}	
-		});
-		//Burger Animation
-		burger.classList.toggle('toggle');
+		if(!menuOpen){
+			nav.classList.toggle('nav-active');
+			//Burger Animation
+			burger.classList.toggle('toggle');
+			menuOpen = true;
+		}else{
+			nav.classList.toggle('nav-active');
+			//Burger Animation
+			burger.classList.toggle('toggle');
+			menuOpen = false;
+		}		
 	});
-
-
 }
 navSlide();
 
+			/*
+			//Animate Links
+			navLinks.forEach((link, index) => {
+			if(link.style.animation){
+				link.style.animation = '';
+			} else {
+				link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+			}	
+			});
+			*/
 /*
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;

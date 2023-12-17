e5bd3ddeabadd05d466798fa73110d9802b4a118
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-12-17 11:10:00 -0800

    Update favicon and navbar styling

--- luis-resume/src/components/common/Navbar.js
+++ luis-resume/src/components/common/Navbar.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import myLG from '../../assets/images/myLG.png';
 import '../../assets/styles/Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
+            <img src={myLG} alt="My Logo" className="navbar-logo" />
             <ul className="nav-links">
                 <li>
-                    <NavLink to="/" exact activeClassName="active">Home</NavLink>
+                    <NavLink to="/" exact activeClassName="active">
+                        Home
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/software" activeClassName="active">Software</NavLink>
+                    <NavLink to="/software" activeClassName="active">
+                        Software
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/embedded" activeClassName="active">Embedded</NavLink>
+                    <NavLink to="/embedded" activeClassName="active">
+                        Embedded
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/ai" activeClassName="active">AI</NavLink>
+                    <NavLink to="/ai" activeClassName="active">
+                        AI
+                    </NavLink>
                 </li>
             </ul>
         </nav>

177c281473f8cdf9f9ddcf16ff5d69e9d3f6a4f5
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-12-18 01:52:26 -0800

    Add React Router to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './assets/styles/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </Router>
     </React.StrictMode>
 );

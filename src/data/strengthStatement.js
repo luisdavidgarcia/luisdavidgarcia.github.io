f16e9fb2501ef14327b3f730f32dae530c059042
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-06-27 19:29:51 -0700

    Remove unused styles and components related to Navbar, RecentWorks, Skills, and other sections. Delete associated CSS files and data files for AI, Embedded, and Software projects. Introduce a new tasks.py file for site management and deployment using Pelican.

--- src/data/strengthStatement.js
+++
@@ -1,5 +0,0 @@
-const strengthStatement = `I prioritize the ability to take ownership of my 
-projects since I enjoy understanding them at a fundamental level, but also it 
-lets me better serve those who will use them.`;
-
-export default strengthStatement;
